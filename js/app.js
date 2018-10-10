6a7763e57dc733e0bd72c7897456733c1459a321
Joanna Baile <joanna@studentsofferingsupport.org>
2018-10-10 17:23:06 -0400

    fixing fetch data

--- js/app.js
+++ js/app.js
@@ -7,10 +7,8 @@
 	var listColours = document.querySelectorAll(".colourCircle");
 	var imageShown = document.querySelector("#show");
     //console.log(listColours);
-
   var TopBtn = document.querySelector('#backToTop');
-
-  var showInfo = document.querySelector(".productImage");
+  var learnMore = document.querySelector('#learn');
   var details = document.querySelector(".products");
   
   //functions
@@ -38,41 +36,42 @@
       setTimeout(backToTop, 0);
     }
   }
-  
-  function getResults() {
-    details.style.display = "block";
-    console.log('hit get results');
-    const dataContainer = document.querySelector('.products');
-    fetch('php/product/read.php')
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-      data.forEach((item, index) => {
-        let colorChips = document.createElement('ol');
-        //create the color chips
-        item.colors.forEach(color => {
-          colorChips.innerHTML += `<li>
-                                      <span>${color.color}</span>
-                                      <span class="chip" style="background-color: ${color.color}"></span>
-                                   </li>`;
-        });
-        // create the wrapper
-        let newItem = document.createElement('li');
-        newItem.innerHTML = `<span>${item.product_name}</span>
-                             <span>${item.product_description}</span>
-                             <span>${item.product_price}</span>`;
-        // add it to the UL
-        newItem.appendChild(colorChips);
-        dataContainer.appendChild(newItem);
-      });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-  }
-  // fire off the fetch call
 
-    getResults();
+  //Gets product info on click
+  function getInfo(){
+    function getResults() {
+      details.style.display = "block";
+      console.log('hit get results');
+      const dataContainer = document.querySelector('.products');
+      fetch('php/product/read.php')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        data.forEach((item, index) => {
+          let colorChips = document.createElement('ol');
+          //create the color chips
+          item.colors.forEach(color => {
+            colorChips.innerHTML += `<li>
+                                        <span>${color.color}</span>
+                                     </li>`;
+          });
+          // create the wrapper
+          let newItem = document.createElement('div');
+          newItem.innerHTML = `<p>${item.product_description}</p>
+                               <p>${item.product_price}</p>`;
+          // add it to the UL
+          newItem.appendChild(colorChips);
+          dataContainer.appendChild(newItem);
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    }
+    // fire off the fetch call
+      getResults();
+      learnMore.removeEventListener('click', getInfo)
+  }
 
 //functions
 
@@ -80,6 +79,8 @@
 	for(var i=0; i<listColours.length; i++){
 		listColours[i].addEventListener("click", changeProduct, false);
   }
+
+  learnMore.addEventListener('click', getInfo);
   
   window.addEventListener('scroll', trackScroll);
   TopBtn.addEventListener('click', backToTop);
